b486368a1af4ce0d6892a8bbaa05649a659d1253
Ashish Nayak <ashish04021989@gmail.com>
2020-06-23 16:31:21 -0500

    Adding Category model and added relationship with Recipe

---
+++ src/main/java/geekstartup/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,47 @@
+package geekstartup.springframework.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+/**
+ * Created by ashishnayak on 23-Jun-2020||3:37 PM
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
