38e4ca02b4572dabea87aab034a6a302f227d689
Ashish Nayak <ashish04021989@gmail.com>
2020-06-29 20:24:24 -0500

    Introducing Junit and Mockito

---
+++ src/test/java/geekstartup/springframework/spring5recipeapp/services/RecipeServiceImplTest.java
@@ -0,0 +1,48 @@
+package geekstartup.springframework.spring5recipeapp.services;
+
+import geekstartup.springframework.spring5recipeapp.domain.Recipe;
+import geekstartup.springframework.spring5recipeapp.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by ashishnayak on 29-Jun-2020||8:06 PM
+ */
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+        HashSet recipeData = new HashSet();
+        recipeData.add(recipe);
+
+        when(recipeRepository.findAll()).thenReturn(recipeData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+        assertEquals(recipes.size(), 1);
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
