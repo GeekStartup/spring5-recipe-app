64c6317803a6d06269ca670fc4b9efd55f32c3a8
Ashish Nayak <ashish04021989@gmail.com>
2020-07-05 00:24:10 -0500

    Added Controller method and html for SaveorUpdate Recipe

--- src/main/java/geekstartup/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/geekstartup/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package geekstartup.springframework.spring5recipeapp.controllers;
 
+import geekstartup.springframework.spring5recipeapp.commands.RecipeCommand;
 import geekstartup.springframework.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -24,6 +27,20 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.getRecipeById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+
+        model.addAttribute("recipeCommand", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("/recipe")
+    public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand recipeCommand) {
+
+        RecipeCommand savedRecipeCommand = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:/recipe/show/" + savedRecipeCommand.getId();
     }
 
 }
